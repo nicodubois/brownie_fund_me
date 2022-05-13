f1e657b4dc16af8f2e0d4214a98c4d359f58bce5
unknown <Nico>
2022-05-12 22:35:53 -0300

    initial commit

---
+++ scripts/deploy.py
@@ -0,0 +1,25 @@
+from doctest import ELLIPSIS_MARKER
+from brownie import FundMe,network,config,MockV3Aggregator, web3
+from scripts.helpful_scripts import get_account,deploy_mocks,LOCAL_BLOCKCHAIN_ENVIRONMNETS
+
+
+def deploy_fund_me():
+    account = get_account()
+    print(account)
+
+    if(network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMNETS):
+        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"];
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(price_feed_address,
+    {"from" : account}, publish_source= config["networks"][network.show_active()]["verify"])
+    print(f"Contract deployed to {fund_me.address}")
+    return fund_me
+
+def main():
+    deploy_fund_me()
+
+
+
